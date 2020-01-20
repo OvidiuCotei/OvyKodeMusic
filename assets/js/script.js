f55eb48f16df702c0a1699e5328644267e6da411
OvidiuCotei <ovidiu_metallica@yahoo.com>
2020-01-20 13:28:28 +0200

    update email in database

--- assets/js/script.js
+++ assets/js/script.js
@@ -219,6 +219,6 @@ function updateEmail(emailClass)
     var emailValue = $("." + emailClass).val();
 
     $.post("includes/handlers/ajax/updateEmail.php", {email: emailValue, username: userLoggedIn}).done(function(response){
-        $("." + emailClass).nextUntil(".message").text(response);
+        $("." + emailClass).nextAll(".message").text(response);
     })
 }
